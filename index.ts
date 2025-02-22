30c861d0b78400e46f490dcfc93a75c041b54416
citynursingcollegegurdaspur <madaanabhay9@gmail.com>
2025-02-23 00:23:38 +0530

    fix: allow all origins in CORS configuration

--- index.ts
+++ index.ts
@@ -42,7 +42,8 @@ app.use(cors({
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(null, true)
+      // callback(new Error('Not allowed by CORS'))
     }
   }
 }))
